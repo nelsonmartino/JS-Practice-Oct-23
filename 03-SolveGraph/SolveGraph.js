499f626f0ee6199dc68b3b6b4eeda8df22f075fd
nelsonmartino <nelsonmartino@gmail.com>
2023-11-01 17:56:15 -0300

    ex. 03 completed

--- 03-SolveGraph/SolveGraph.js
+++ 03-SolveGraph/SolveGraph.js
@@ -1,5 +1,11 @@
 function SolveGraph(graph, start, target) {
   // Tu código aquí
+  if (!graph[start].length) return false;
+  if (graph[start].includes(target)) {
+    return true;
+  } else {
+    return SolveGraph(graph, graph[start].pop(), target);
+  }
 }
 
 module.exports = SolveGraph;
