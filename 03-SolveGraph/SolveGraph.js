5f501011eda0170180867d4bf509c388fb41c320
nelsonmartino <nelsonmartino@gmail.com>
2023-11-02 12:15:49 -0300

    mod ex 3, added ex 4

--- 03-SolveGraph/SolveGraph.js
+++ 03-SolveGraph/SolveGraph.js
@@ -1,11 +1,23 @@
-function SolveGraph(graph, start, target) {
-  // Tu código aquí
-  if (!graph[start].length) return false;
-  if (graph[start].includes(target)) {
-    return true;
-  } else {
-    return SolveGraph(graph, graph[start].pop(), target);
+function SolveGraph(graph, start, target, visited = {}) {
+  //! Tu código aquí
+  // if (!graph[start].length) return false;
+  // if (graph[start].includes(target)) {
+  //   return true;
+  // } else {
+  //   return SolveGraph(graph, graph[start].pop(), target);
+  // }
+  // ! Solución propuesta por Ivan Reyes
+  if (visited[start]) {
+    return false;
+  } else visited[start] = true;
+
+  for (const node of graph[start]) {
+    if (node === target) {
+      return true;
+    }
+    if (SolveGraph(graph, node, target, visited)) return true;
   }
+  return false;
 }
 
 module.exports = SolveGraph;
--- 04-ClockMinuteAdder/ClockMinuteAdder.js
+++ 04-ClockMinuteAdder/ClockMinuteAdder.js
@@ -1,5 +1,23 @@
 function clockMinuteAdder(time, minutesToAdd) {
   // Tu código aquí
+  //* Separo y convierto a formato numérico todos los datos de entrada
+  let minutes = Number(time.split(':')[1]);
+  let hours = Number(time.split(':')[0]);
+  minutesToAdd = Number(minutesToAdd);
+  //* Obtengo las horas a agregar
+  const hoursToAdd = Math.floor((minutesToAdd + minutes) / 60);
+  //*Obtengo los minutos resultantes de la hora
+  minutes = (minutes + minutesToAdd) % 60;
+  //* Obtengo las horas resultantes en formato de 12 horas
+  hours = (hours + hoursToAdd) % 12;
+  //* Si los minutos son de un solo dígito, agrego un cero a la izquierda
+  if (minutes < 10) minutes = '0' + minutes.toString();
+  //* No existen las 0 horas, en su lugar existen las 12 horas
+  if (hours === 0) hours = '12';
+  //* Si las horas son de un solo dígito, agrego un cero a la izquierda
+  if (hours < 10) hours = '0' + hours.toString();
+
+  return `${hours}:${minutes}`;
 }
 
 module.exports = clockMinuteAdder;
